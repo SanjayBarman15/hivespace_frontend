36867b565f35ac04a70461d9bb982f928a301fae
SanjayBarman15 <sanjaybarman1179@gmail.com>
2026-04-22 22:19:47 +0530

    feat: implement global navigation rail and dashboard layout with mail module support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
-
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
-
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
 
 export default function RootLayout({
   children,
@@ -20,9 +11,9 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+      className="antialiased"
     >
-      <body>
+      <body className="font-sans bg-[#0E0E10]">
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
